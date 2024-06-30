873bb7de57abd2f82d6c93a7668481a137d0fb65
ouard1 <121223618+ouard1@users.noreply.github.com>
2024-06-30 19:26:33 +0100

    Update settings.py

--- adamim_backend/adamim_backend/settings.py
+++ adamim_backend/adamim_backend/settings.py
@@ -61,7 +61,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',  
     'http://192.168.43.2:8080',
-    'https://adamim-ocr-django.vercel.app/'
+    'https://adamim-ocr-django.vercel.app/',
+    'https://adamim-ocr.vercel.app/'
 ]
 
 # Security headers
